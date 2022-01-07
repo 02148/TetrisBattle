846fe5b85e2488e8ada7eacecb34f55678403ca5
Niels Raunkjær <nielsraunkjaer@gmail.com>
2022-01-07 12:12:52 +0100

    Rename game to gamesession

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,8 +5,6 @@ import org.jspace.RemoteSpace;
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -19,13 +17,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        btn.setOnAction(event -> System.out.println("Hello World!"));
 
         StackPane root = new StackPane();
         root.getChildren().add(btn);
@@ -36,6 +28,7 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
     public static void main(String[] args) throws IOException {
         SpaceRepository repo = new SpaceRepository();
         repo.addGate("tcp://server:6969/?keep");
