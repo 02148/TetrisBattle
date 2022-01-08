83344a6f21decb0ec60ac44c4f14a2a9dbe6f68b
propelKarlos <PropelKarlos@gmail.com>
2022-01-08 12:27:18 +0100

    Board Updates - Temporary changes in client.java

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,4 +1,51 @@
 package Client;
 
-public class Client {
+import Client.UI.Board;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import org.jspace.SpaceRepository;
+
+import java.io.IOException;
+
+
+// Right now a lot of crap is included - this is only used for debugging end development purposes - Magn.
+public class Client extends Application {
+
+  @Override
+  public void start(Stage primaryStage) {
+    Button btn = new Button();
+    btn.setText("Say 'Hello World' to client");
+    btn.setOnAction(new EventHandler<ActionEvent>() {
+
+      @Override
+      public void handle(ActionEvent event) {
+        System.out.println("Hello World!");
+      }
+    });
+
+    StackPane root = new StackPane();
+    root.getChildren().add(btn);
+
+    Board nBoard = new Board(30, 30, 400);
+    root.getChildren().add(nBoard);
+
+    Scene scene = new Scene(root, 750, 500);
+
+    primaryStage.setTitle("Hello World!");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+  public static void main(String[] args) throws IOException {
+    SpaceRepository repo = new SpaceRepository();
+    repo.addGate("tcp://server:6969/?keep");
+
+    launch(args);
+  }
 }
--- src/Client/UI/Board.java
+++ src/Client/UI/Board.java
@@ -1,53 +1,62 @@
 package Client.UI;
 
+import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-public class Board extends GridPane {
+public class Board extends Pane {
     private int posX, posY, width, height;
+    private GridPane grid;
 
-    public Board(int posX, int posY, int width, int height) {
+    public Board(int posX, int posY, int size) {
         super();
 
-        this.setWidth(400);
-        this.setHeight(800);
-        this.setLayoutX(posX);
-        this.setLayoutY(posY);
+        grid = new GridPane();
+
+        grid.setLayoutX(posX);
+        grid.setLayoutY(posY);
 
         for(int y = 0; y < 20; y++) {
             for (int x = 0; x < 10; x++) {
                 Canvas canvas = new Canvas(20,20);
-                GraphicsContext gc = canvas.getGraphicsContext2D();
-                gc.setFill(Color.BLACK);
-                gc.fillRect(0,0,20,20);
-                gc.setFill(Color.BEIGE);
-
-                if(x == 0) {
-                    if(y == 19) {
-                        gc.fillRect(1,1,19,19);
-                    } else {
-                        gc.fillRect(1,1,19,19);
-                    }
-                } else if(x == 9) {
-                    if (y == 19) {
-
-                    } else {
-                        gc.fillRect(1,1,18,19);
-                    }
-                } else {
-                    gc.fillRect(1,1,19,19);
-                }
-
-
-                this.add(canvas, x, y);
+                updateBlock(x, y, Color.BEIGE, canvas.getGraphicsContext2D());
+                grid.add(canvas, x, y);
             }
         }
 
+        this.getChildren().add(grid);
+    }
 
-        this.width = width;
-        this.height = height;
+    private void updateBlock(int x, int y, Color color, GraphicsContext gc) {
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0,0,20,20);
+        gc.setFill(color);
+
+        if(x == 9 && y == 19) {
+            gc.fillRect(1,1,18,18);
+        } else if(y == 19) {
+            gc.fillRect(1,1,19,18);
+        } else if(x == 9) {
+            gc.fillRect(1,1,18,19);
+        } else {
+            gc.fillRect(1,1,19,19);
+        }
+    }
+
+    public void setBlockColor(int index, Color color) {
+        int y = index / 10;
+        int x = index % 10;
+        setBlockColor(x, y, color);
+    }
+
+    public void setBlockColor(int x, int y, Color color) {
+        int index = y*10+x;
+        Canvas block = (Canvas) grid.getChildren().get(index);
+        updateBlock(x, y, color, block.getGraphicsContext2D());
     }
 }
