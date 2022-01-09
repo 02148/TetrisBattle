4e5c8d4be63c374924b79733f8e9f692c739757f
Niels Raunkjær <nielsraunkjaer@gmail.com>
2022-01-09 14:55:57 +0100

    Tweak board.toString() for windows and create toColorCode() byte method for mino

--- src/Client/Models/Mino.java
+++ src/Client/Models/Mino.java
@@ -6,10 +6,33 @@ public class Mino {
   public int posX, posY;
   public Color color;
   public boolean isPlaced;
+
   public Mino(int x, int y, Color color, boolean isPlaced) {
     this.posX = x;
     this.posY = y;
     this.color = color;
     this.isPlaced = isPlaced;
+  }
+
+  public byte toColorCode() {
+    String c;
+
+    if (this.color.equals(Color.BLUE))
+      c = "0";
+    else if(this.color.equals(Color.CYAN))
+     c = "1";
+    else if(this.color.equals(Color.ORANGE))
+     c = "2";
+    else if(this.color.equals(Color.YELLOW))
+     c = "3";
+    else if(this.color.equals(Color.GREEN))
+     c = "4";
+    else if(this.color.equals(Color.PINK))
+     c = "5";
+    else if(this.color.equals(Color.RED))
+     c = "6";
+    else c = "7";
+
+    return Byte.parseByte(c);
   }
 }
