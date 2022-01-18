527010539b5305643643c58572a0705fc3104c18
PrimtalPrimdahl <s204471@student.dtu.dk>
2022-01-18 16:00:15 +0100

    more colors

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -41,10 +41,10 @@ public class Client extends Application {
   }
   public void main(String[] args) throws IOException {
     //SpaceRepository repo = new SpaceRepository();
-    //repo.addGate("tcp://localhost:6969/?mainServer");
-    mainServer = new RemoteSpace("tcp://localhost:6969/main?mainServer");
-    userToServer = new RemoteSpace("tcp://localhost:6969/userToServer?conn");
-    serverToUser = new RemoteSpace("tcp://localhost:6969/serverToUser?conn");
+    //repo.addGate("tcp://10.209.222.2:6969/?mainServer");
+    mainServer = new RemoteSpace("tcp://10.209.222.2:6969/main?mainServer");
+    userToServer = new RemoteSpace("tcp://10.209.222.2:6969/userToServer?conn");
+    serverToUser = new RemoteSpace("tcp://10.209.222.2:6969/serverToUser?conn");
 
     //launch(args);
   }
@@ -60,7 +60,7 @@ public class Client extends Application {
       if (loginResponse[1].equals("ok")) {
         UUID = (String) loginResponse[2];
         System.out.println("Logged in response got from server");
-        chatSpace = new RemoteSpace("tcp://localhost:4242/globalChat?conn");
+        chatSpace = new RemoteSpace("tcp://10.209.222.2:4242/globalChat?conn");
       } else {
         //Error message
       }
@@ -88,7 +88,7 @@ public class Client extends Application {
         roomUUID = (String) roomResponse[2];
         System.out.println("Room can be started by UI");
 
-        chatSpace = new RemoteSpace("tcp://localhost:4242/" + roomUUID + "?conn");
+        chatSpace = new RemoteSpace("tcp://10.209.222.2:4242/" + roomUUID + "?conn");
 
         //Room can be started by UI
         System.out.println("Room can be joined using " + roomResponse[3]);
@@ -120,7 +120,7 @@ public class Client extends Application {
 
       if (roomResponse[1].equals("ok")) {
         roomUUID = (String) roomResponse[2];
-        chatSpace = new RemoteSpace("tcp://localhost:4242/" + roomUUID + "?conn");
+        chatSpace = new RemoteSpace("tcp://10.209.222.2:4242/" + roomUUID + "?conn");
         //Room can be started by UI
         System.out.println("Trying to join room");
 
