e714cf9b9395036c8a3e4fbdf46e249cee754a64
Niels Raunkjær <nielsraunkjaer@gmail.com>
2022-01-19 10:12:09 +0100

    Add get random player logic in engine

--- src/MainServer/GameSession/Combat/Engine.java
+++ src/MainServer/GameSession/Combat/Engine.java
@@ -1,4 +1,41 @@
 package MainServer.GameSession.Combat;
 
-public class Engine {
+import org.jspace.FormalField;
+import org.jspace.Space;
+
+import java.util.HashMap;
+import java.util.Random;
+
+public class Engine implements Runnable {
+    Random rnd;
+    Space conns;
+    HashMap<Integer, Integer> linesSentStats;
+
+
+    public Engine(Space conns) {
+        this.rnd = new Random();
+        this.linesSentStats = new HashMap<>();
+        this.conns = conns;
+    }
+
+    public int getRandomPlayerIndex() {
+        int idx = -1;
+        try {
+            var noOfPlayers = (long) this.conns
+                    .getAll(new FormalField(String.class))
+                    .size();
+
+            idx = (int) (rnd.nextInt() * noOfPlayers);
+        } catch (Exception e) { e.printStackTrace();}
+
+        return idx;
+    }
+
+
+
+
+    @Override
+    public void run() {
+
+    }
 }
