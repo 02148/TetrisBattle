f827d472ef81ba86a36bf68029521248f1921154
Magnus Meyer <magnus.fjellerup@gmail.com>
2022-01-19 10:35:02 +0100

    very small change

--- src/common/Constants.java
+++ src/common/Constants.java
@@ -2,7 +2,7 @@ package common;
 
 import javafx.scene.paint.Color;
 
-public abstract class Constants {
-    public static final String IP_address = "localhost";
+public class Constants {
+    public static final String IP_address = "10.209.222.2";
     public static final Color[] tetroColors = new Color[]{Color.CYAN, Color.DARKSLATEBLUE, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.DARKMAGENTA, Color.FIREBRICK};
 }
